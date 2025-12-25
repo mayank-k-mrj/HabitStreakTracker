5e05b87465e023dda0012d30f6cbb2f9dfaac4e6
Mayank Kumar <mayankcicia@gmail.com>
2025-12-25 22:13:55 +0530

    Trying to fix the deployment issue

--- src/main/java/com/habit_streak_tracker/habit_streak_tracker/HabitStreakTrackerApplication.java
+++ src/main/java/com/habit_streak_tracker/habit_streak_tracker/HabitStreakTrackerApplication.java
@@ -1,13 +1,26 @@
 package com.habit_streak_tracker.habit_streak_tracker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class HabitStreakTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HabitStreakTrackerApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(Environment env) {
+		return args -> {
+			System.out.println("--- DEBUG ENVIRONMENT ---");
+			System.out.println("MYSQLHOST: " + env.getProperty("MYSQLHOST"));
+			System.out.println("PORT: " + env.getProperty("PORT"));
+			// Don't print passwords for security!
+			System.out.println("-------------------------");
+		};
 	}
 
 }

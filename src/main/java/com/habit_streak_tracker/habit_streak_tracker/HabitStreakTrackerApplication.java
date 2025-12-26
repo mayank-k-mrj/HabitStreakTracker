39b76a842a1c76ac7094288464f82b7044295382
Mayank Kumar <mayankcicia@gmail.com>
2025-12-26 21:53:31 +0530

    Deleted unnecessar code

--- src/main/java/com/habit_streak_tracker/habit_streak_tracker/HabitStreakTrackerApplication.java
+++ src/main/java/com/habit_streak_tracker/habit_streak_tracker/HabitStreakTrackerApplication.java
@@ -12,15 +12,5 @@ public class HabitStreakTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HabitStreakTrackerApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner commandLineRunner(Environment env) {
-		return args -> {
-			System.out.println("--- DEBUG ENVIRONMENT ---");
-			System.out.println("MYSQLHOST: " + env.getProperty("MYSQLHOST"));
-			System.out.println("PORT: " + env.getProperty("PORT"));
-			// Don't print passwords for security!
-			System.out.println("-------------------------");
-		};
-	}
 
 }
